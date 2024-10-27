ec2883a1d3bd435e193333d2faa55e95748f4364
ZacharyTa <Zachary.C.Ta@gmail.com>
2024-10-27 16:35:38 -0700

    Accessing progress-overview and recommended-learnining now requires users to be logged in. Next up is adding a check to see if user hasAccess to the content they want to access

--- app/recommended-learning-path/layout.tsx
+++ app/recommended-learning-path/layout.tsx
@@ -1,10 +1,24 @@
 import { ReactNode } from "react";
+import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import config from "@/config";
 
-export default function RecommendedLearningPathPageLayout({
+export default async function RecommendedLearningPathPageLayout({
   children,
 }: {
   children: ReactNode;
 }) {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) {
+    redirect(config.auth.loginUrl);
+  }
+
   return (
     <div className="recommended-learning-path-page-layout">
       {/* Add any shared UI elements here, such as a sidebar or header */}
